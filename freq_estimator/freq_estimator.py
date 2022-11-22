6aa4025a1eda5ec7aaaf7a71285e4204c2c53c83
YusenRan <wired.ran@gmail.com>
2022-11-22 23:35:37 +0900

    [clean]

--- freq_estimator/freq_estimator.py
+++ freq_estimator/freq_estimator.py
@@ -11,6 +11,17 @@ def exp_decay_sinusoid(z_omega : Tensor, z_phi :Tensor, n : int) -> Tensor:
     sinusoid = torch.cos(arr_n*z_omega.angle() + z_phi.angle())
     return magnitude * sinusoid
 
+def create_target_sinusoid(signal_length :int) -> Tensor:
+    # 目標正弦波を作成
+    # 周波数・振幅・初期位相ともにランダム
+    target_freq = torch.rand(1)*torch.pi
+    target_mag = torch.rand(1)
+    target_phase = torch.rand(1)*torch.pi
+    target_sinusoid = torch.cos(torch.arange(signal_length) * target_freq + target_phase) * target_mag
+
+    print(f"Target freq/mag/phase: {target_freq.item():.4f} / {target_mag.item():.4f} / {target_phase.item():.4f}")
+    return target_sinusoid
+
 def estimate_freq():
     # 複素数パラメータ初期化
     # 単位円上の適当な値で初期化します。
@@ -19,17 +30,11 @@ def estimate_freq():
     param_omega = torch.exp(init_freq*1.0j).requires_grad_()    # 角周波数・減衰係数パラメーター
     param_phi = torch.exp(init_phase*1.0j).requires_grad_()     # 振幅・初期位相パラメーター
 
-    # 目標正弦波を作成
-    # 長さ4096、周波数・振幅・初期位相ともにランダム
-    signal_length=4096
-    target_freq = torch.rand(1)*torch.pi
-    target_mag = torch.rand(1)
-    target_phase = torch.rand(1)*torch.pi
-    target_sinusoid = torch.cos(torch.arange(signal_length) * target_freq + target_phase) * target_mag
+    signal_length = 4096
+    target_sinusoid = create_target_sinusoid(signal_length)
 
-    # 初期・目標パラメーターをプリント
+    # 初期パラメーターをプリント
     print(f"Initial freq/mag/phase: {param_omega.angle().item():.4f} / {param_phi.abs().item():.4f} / {param_phi.angle().item():.4f}")
-    print(f"Target freq/mag/phase: {target_freq.item():.4f} / {target_mag.item():.4f} / {target_phase.item():.4f}")
 
     # 勾配降下ループ
     optimizer = torch.optim.Adam([param_omega, param_phi], lr=1e-4)
