ee7261ec665ac2f01b5e42d4a89cf230e043b0ab
YusenRan <wired.ran@gmail.com>
2022-11-23 13:19:28 +0900

    [change]double sin

--- freq_estimator/freq_estimator.py
+++ freq_estimator/freq_estimator.py
@@ -1,7 +1,7 @@
-# ライブラリ読込
+from dataclasses import dataclass
+
+
 import matplotlib.pyplot as plt
-
-
 import torch
 import torch.nn.functional as F
 from torch import Tensor
@@ -40,20 +40,29 @@ def estimate_freq():
     # 単位円上の適当な値で初期化します。
     init_freq = torch.rand(1)*torch.pi
     init_phase = torch.rand(1)*torch.pi
+    init_freq2 = torch.rand(1)*torch.pi
+    init_phase2 = torch.rand(1)*torch.pi
     param_omega = torch.exp(init_freq*1.0j).requires_grad_()    # 角周波数・減衰係数パラメーター
     param_phi = torch.exp(init_phase*1.0j).requires_grad_()     # 振幅・初期位相パラメーター
+    param_omega2 = torch.exp(init_freq2*1.0j).requires_grad_()    # 角周波数・減衰係数パラメーター
+    param_phi2 = torch.exp(init_phase2*1.0j).requires_grad_()     # 振幅・初期位相パラメーター
 
     signal_length = 4096
-    target_sinusoid = create_target_sinusoid(signal_length)
+    target_sinusoid1 = create_target_sinusoid(signal_length)
+    target_sinusoid2 = create_target_sinusoid(signal_length)
+    target_sinusoid = target_sinusoid1 + target_sinusoid2
 
     # 初期パラメーターをプリント
     print(f"Initial freq/mag/phase: {param_omega.angle().item():.4f} / {param_phi.abs().item():.4f} / {param_phi.angle().item():.4f}")
+    print(f"Initial freq/mag/phase: {param_omega2.angle().item():.4f} / {param_phi2.abs().item():.4f} / {param_phi2.angle().item():.4f}")
 
     # 勾配降下ループ
-    optimizer = torch.optim.Adam([param_omega, param_phi], lr=1e-4)
+    optimizer = torch.optim.Adam([param_omega, param_phi, param_omega2, param_phi2], lr=1e-4)
     for _ in tqdm.tqdm(range(50000),desc="Gradient descent"):
         # パラメーターから減衰正弦波を生成
-        estimated_sinusoid = exp_decay_sinusoid(param_omega, param_phi, signal_length)
+        estimated_sinusoid1 = exp_decay_sinusoid(param_omega, param_phi, signal_length)
+        estimated_sinusoid2 = exp_decay_sinusoid(param_omega2, param_phi2, signal_length)
+        estimated_sinusoid = estimated_sinusoid1 + estimated_sinusoid2
         # 損失関数：Mean Squared Error
         loss = F.mse_loss(estimated_sinusoid, target_sinusoid)
         optimizer.zero_grad()
@@ -62,9 +71,12 @@ def estimate_freq():
 
     print(f"Loss: {loss.item()}")
     print(f"Estimated freq/mag/phase: {param_omega.angle().item():.4f} / {param_phi.abs().item():.4f} / {param_phi.angle().item():.4f}")
+    print(f"Estimated2 freq/mag/phase: {param_omega2.angle().item():.4f} / {param_phi2.abs().item():.4f} / {param_phi2.angle().item():.4f}")
 
-    estimated_sinusoid = torch.cos(torch.arange(signal_length) * param_omega.angle() + param_phi.angle()) * param_phi.abs()
-    draw2compare(target_sinusoid[:100], estimated_sinusoid.detach().numpy()[:100])
+    estimated_sinusoid1 = torch.cos(torch.arange(signal_length) * param_omega.angle() + param_phi.angle()) * param_phi.abs()
+    estimated_sinusoid2 = torch.cos(torch.arange(signal_length) * param_omega2.angle() + param_phi2.angle()) * param_phi2.abs()
+    estimated_sinusoid = estimated_sinusoid1 + estimated_sinusoid2
+    draw2compare(target_sinusoid[:300], estimated_sinusoid.detach().numpy()[:300])
 
 if __name__ == "__main__":
     estimate_freq()
